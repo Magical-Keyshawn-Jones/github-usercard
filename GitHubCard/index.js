131bd33126f3a09c85901b7299692aba65d8e53f
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-03-10 19:34:43 -0600

    finished finding my data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,17 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const awesomeData = []
 
+axios.get('https://api.github.com/users/Magical-Keyshawn-Jones')
+.then(object => awesomeData.push(object.data))
+.catch(err => console.log('error!', err));
+
+console.log(awesomeData);
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
